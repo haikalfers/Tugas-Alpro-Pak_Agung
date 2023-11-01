62515b5a06cf87de43e759b3a86337fbda0d7e87
Haikal Ferdian <100331750+haikalfers@users.noreply.github.com>
2023-11-01 13:05:31 +0700

    Create tugas_alpro_pizza.py

---
+++ tugas_alpro_pizza.py
@@ -0,0 +1,9 @@
+"""
+Kelas : 2023E
+Prodi : D4 Manajemen Informatika
+Kelompok 4
+Anggota Kelompok
+1. Zaidan Dhiya Ulhaq (23091397152)
+2. Alya Faadhilah Putri (23091397153)
+3. Haikal Ferdian Saputra (23091397154)
+"""
