f01c61fbf9c4182cf851486cea119a400b0882f2
Haikal Ferdian <100331750+haikalfers@users.noreply.github.com>
2023-11-01 13:10:18 +0700

    Update tugas_alpro_pizza.py

--- tugas_alpro_pizza.py
+++ tugas_alpro_pizza.py
@@ -7,3 +7,23 @@ Anggota Kelompok
 2. Alya Faadhilah Putri (23091397153)
 3. Haikal Ferdian Saputra (23091397154)
 """
+
+total_harga : 0
+
+print(
+    """
+    1. Frankfuter BBQ
+    2. Meat Monsta
+    3. Super Supreme
+    4. Super Supreme Chicken
+    """)
+
+topping_pizza = int(input("Pilih Topping Pizza (piih angka): "))
+if topping_pizza == 1:
+    topping_pizza = "Frankfuter BBQ"
+if topping_pizza == 2:
+    topping_pizza = "Meat Monsta"
+if topping_pizza == 3:
+    topping_pizza = "Super Supreme"
+if topping_pizza == 4:
+    topping_pizza = "Super Supreme Chicken"
